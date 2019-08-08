eaaf0d93c3c17d099245d56974de9db38b09ce7f
Philip Ridgeway <pridgeway@esri.com>
2019-08-08 14:37:53 -0700

    Fix parameter name in documentation comments

--- data-collection/data-collection/Extensions/UIKit/UIAlertController/UIAlertController+Alert.swift
+++ data-collection/data-collection/Extensions/UIKit/UIAlertController/UIAlertController+Alert.swift
@@ -44,7 +44,7 @@ extension UIAlertController {
     ///   - actionTitle: The title of the alert's action button.
     ///   - action: A closure triggered upon tapping the action button (optional).
     ///   - isDestructive: A `Bool` specifiying if the action button should be configured as destructive (optional), the default value is `false`.
-    ///   - cancel: A closure triggered upon tapping the cancel button (optional).
+    ///   - cancelHandler: A closure triggered upon tapping the cancel button (optional).
     ///
     /// - Returns: A newly created and configured two-button button `UIAlertController` of style `.alert`.
     static func multiAlert(title: String? = nil, message: String, actionTitle: String, action: ((UIAlertAction) -> Void)? = nil, isDestructive: Bool = false, cancel cancelHandler: ((UIAlertAction) -> Void)? = nil) -> UIAlertController {
