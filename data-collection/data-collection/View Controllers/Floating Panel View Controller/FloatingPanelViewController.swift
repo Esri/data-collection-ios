185a351cc93a828773f6e6a413ac05e025486516
Mark Dostal <mhdostal@aol.com>
2020-07-07 13:45:53 -0500

    Create floating panel files and some code to show the vc

---
+++ data-collection/data-collection/View Controllers/Floating Panel View Controller/FloatingPanelViewController.swift
@@ -0,0 +1,41 @@
+//
+// Copyright 2020 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import UIKit
+
+class FloatingPanelViewController: UIViewController {
+
+    var topLayoutConstraint: NSLayoutConstraint?
+    
+    /// The insets from the edge of the screen. The client can override the insets for various layout options.
+    let edgeInsets = UIEdgeInsets(top: 8.0, left: 8.0, bottom: 8.0, right: 8.0)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
