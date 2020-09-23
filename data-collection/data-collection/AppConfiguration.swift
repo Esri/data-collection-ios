17a4e6ba22596b467fbc00adbe5b89f17e483e5e
Eli Gregory <egregory@esri.com>
2020-09-23 16:38:00 -0700

    reorganizes AppConfiguration

--- data-collection/data-collection/AppConfiguration.swift
+++ data-collection/data-collection/AppConfiguration.swift
@@ -16,18 +16,26 @@ import ArcGIS
 
 // MARK:- Configurable
 
+// MARK: 1. Portal & Web Map
+
+extension URL {
+    
+    /// The URL to the base portal.
+    /// - Note: A `fatalError` is thrown if a URL can't be built from the configuration.
+    static let basePortal: URL = {
+        guard let url = URL(string: "https://www.arcgis.com") else {
+            fatalError("App Configuration must contain a valid portal service url.")
+        }
+        return url
+    }()
+}
+
 extension String {
     /// The ID of your portal's [web map](https://runtime.maps.arcgis.com/home/item.html?id=16f1b8ba37b44dc3884afc8d5f454dd2).
     static let webMapItemID = "16f1b8ba37b44dc3884afc8d5f454dd2"
-    
-    /// The base portal's domain.
-    /// This is used to both build a `URL` to your portal as well as the base URL string used to check reachability.
-    /// - Note: exclude `http` or `https`, this is configured in `basePortalURL`.
-    static let basePortalDomain = "www.arcgis.com"
-    
-    /// The name of your side-loaded offline Locator.
-    static let offlineLocator = "AddressLocator"
 }
+
+// MARK: 2. OAuth Redirect URL
 
 enum OAuth {
     /// The App's oAuth redirect URL.
@@ -35,7 +43,15 @@ enum OAuth {
     static let redirectUrl: String = "data-collection://auth"
 }
 
+// MARK: 3. Address Locator
+
+extension String {
+    /// The name of your side-loaded offline Locator.
+    static let offlineLocator = "AddressLocator"
+}
+
 extension URL {
+    
     /// The URL to the world geocode service.
     /// Swap out for another geocoder server if you prefer.
     /// - Note: A `fatalError` is thrown if a URL can't be built from the configuration.
@@ -56,17 +72,6 @@ extension OAuth {
             fatalError("OAuth.redirectUrl must contain a valid URL.")
         }
         return components
-    }()
-}
-
-extension URL {
-    /// The URL to the base portal.
-    /// - Note: A `fatalError` is thrown if a URL can't be built from the configuration.
-    static let basePortal: URL = {
-        guard let url = URL(string: "https://\(String.basePortalDomain)") else {
-            fatalError("App Configuration must contain a valid portal service url.")
-        }
-        return url
     }()
 }
 
