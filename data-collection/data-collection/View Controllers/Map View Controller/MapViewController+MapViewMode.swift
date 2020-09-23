938a533dc7f478fd29bc0b5db6d5b7d88f42054b
Mark Dostal <mhdostal@aol.com>
2020-09-23 11:49:57 -0500

    use animation block to display identify results.

--- data-collection/data-collection/View Controllers/Map View Controller/MapViewController+MapViewMode.swift
+++ data-collection/data-collection/View Controllers/Map View Controller/MapViewController+MapViewMode.swift
@@ -18,20 +18,27 @@ extension MapViewController {
     
     func adjustForMapViewMode(from: MapViewMode?, to: MapViewMode) {
         
-        func setIdentifyResultsVisible(_ visible: Bool) {
-            // If we're showing a floating panel...
-            if visible {
-                // Set the selected popups on the identify results view controller.
-                identifyResultsViewController.selectedPopups = selectedPopups
-
-                let floatingPanelItem = identifyResultsViewController.floatingPanelItem
-                floatingPanelItem.title = "Identify Results"
-                
-                let selected = selectedPopups.count
-                floatingPanelItem.subtitle = String("\(selected) Feature\(selected > 1 ? "s" : "")")
-                floatingPanelItem.image = UIImage(named: "feature-details")
-                floatingPanelController = presentFloatingPanel(identifyResultsViewController)
-                floatingPanelController?.delegate = self
+        let identifyResultsVisible: (Bool) -> UIViewAnimations = { [weak self] (visible) in
+            return {
+                guard let self = self else { return }
+                // If we're showing a floating panel...
+                if visible {
+                    // Set the selected popups on the identify results view controller.
+                    self.identifyResultsViewController.selectedPopups = self.selectedPopups
+                    
+                    let floatingPanelItem = self.identifyResultsViewController.floatingPanelItem
+                    floatingPanelItem.title = "Identify Results"
+                    
+                    let selected = self.selectedPopups.count
+                    floatingPanelItem.subtitle = String("\(selected) Feature\(selected > 1 ? "s" : "")")
+                    floatingPanelItem.image = UIImage(named: "feature-details")
+                    self.floatingPanelController = self.presentFloatingPanel(self.identifyResultsViewController)
+                    self.floatingPanelController?.delegate = self
+                }
+                else {
+                    // This will dismiss the floating panel controller.
+                    self.floatingPanelController = nil
+                }
             }
         }
         
@@ -59,50 +66,50 @@ extension MapViewController {
         }
         
         let animations: [UIViewAnimations]
-        var identifyResultsVisible = false
         switch to {
-            
         case .defaultView:
             pinDropView.pinDropped = false
             animations = [ selectViewVisible(false),
+                           identifyResultsVisible(false),
                            mapViewVisible(true) ]
             hideMapMaskViewForOfflineDownloadArea()
 
         case .disabled:
             pinDropView.pinDropped = false
             animations = [ selectViewVisible(false),
+                           identifyResultsVisible(false),
                            mapViewVisible(false) ]
             hideMapMaskViewForOfflineDownloadArea()
             
         case .selectingFeature:
             pinDropView.pinDropped = true
             animations = [ selectViewVisible(true),
+                           identifyResultsVisible(false),
                            mapViewVisible(true) ]
             hideMapMaskViewForOfflineDownloadArea()
             selectViewHeaderLabel.text = "Choose location"
             selectViewSubheaderLabel.text = "Pan & zoom map under pin"
             
-        case .selectedFeature(_):
+        case .selectedFeature(let loaded):
             pinDropView.pinDropped = false
             animations = [ selectViewVisible(false),
+                           identifyResultsVisible(loaded),
                            mapViewVisible(true) ]
-            identifyResultsVisible = true
             hideMapMaskViewForOfflineDownloadArea()
             
         case .offlineMask:
             pinDropView.pinDropped = false
             animations = [ selectViewVisible(true),
+                           identifyResultsVisible(false),
                            mapViewVisible(true) ]
             presentMapMaskViewForOfflineDownloadArea()
             selectViewHeaderLabel.text = "Choose extent"
             selectViewSubheaderLabel.text = "Pan & zoom map within region"
         }
         
-        UIView.animate(withDuration: 0.2, animations: { [weak self] in
+        UIView.animate(withDuration: 0.2) { [weak self] in
             for animation in animations { animation() }
             self?.view.layoutIfNeeded()
-        }) { (finished) in
-            setIdentifyResultsVisible(identifyResultsVisible)
         }
     }
 }
