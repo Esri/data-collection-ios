c076035eeb5ed7346a049664c9614262fbb0755b
Mark Dostal <mhdostal@aol.com>
2020-10-01 08:44:40 -0500

    Tweak and simplify the FloatingPanelPresenter protocol and extension

--- data-collection/data-collection/View Controllers/Map View Controller/MapViewController.swift
+++ data-collection/data-collection/View Controllers/Map View Controller/MapViewController.swift
@@ -58,12 +58,6 @@ class MapViewController: UIViewController {
     
     var extrasNavigationController: UINavigationController?
     var layerContentsViewController: LayerContentsViewController?
-    var floatingPanelController: FloatingPanelController? {
-        willSet {
-            // Dismiss the existing floating panel.
-            dismissFloatingPanel()
-        }
-    }
 
     var mapViewMode: MapViewMode = .defaultView {
         didSet {
