614f3249b863e0da6c9b3cee5c85ecfa1cef1dd5
Eli Gregory <egregory@esri.com>
2020-12-07 09:55:17 -0800

    removes duplicate declaration introduced by rabase

--- data-collection/data-collection/View Controllers/Profile View Controller/ProfileViewController.swift
+++ data-collection/data-collection/View Controllers/Profile View Controller/ProfileViewController.swift
@@ -141,11 +141,6 @@ class ProfileViewController: UITableViewController {
         delegate.profileViewControllerRequestsDownloadMapOfflineOnDemand(profileViewController: self)
     }
     
-    @objc
-    func userRequestsTakeMapOffline() {
-        delegate.profileViewControllerRequestsDownloadMapOfflineOnDemand(profileViewController: self)
-    }
-    
     // MARK:- Portal
     
     @objc
