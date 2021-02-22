14313d6383fbc5a968dbcc4d4575e82099a6c790
Mark Dostal <mhdostal@aol.com>
2021-02-22 11:06:12 -0600

    Don't ever show settings after the user already selected the permissions.

--- data-collection/data-collection/Utilities/ImagePickerPermissions.swift
+++ data-collection/data-collection/Utilities/ImagePickerPermissions.swift
@@ -296,16 +296,13 @@ extension ImagePickerPermissions {
             PHPhotoLibrary.requestAuthorization { (status) in
                 DispatchQueue.main.async {
                     var granted: Bool
-                    var shouldOpenSettings: Bool
                     if #available(iOS 14, *) {
                         granted = PHPhotoLibrary.authorizationStatus(for: .readWrite) == .authorized
-                        shouldOpenSettings = !granted
                     }
                     else {
                         granted = PHPhotoLibrary.authorizationStatus() == .authorized
-                        shouldOpenSettings = false
                     }
-                    completion(granted, shouldOpenSettings)
+                    completion(granted, false)
                 }
             }
         }
