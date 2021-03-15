ff73b0f9a8a1be015dea76cf64a6d136d486ba72
Eli Gregory <egregory@esri.com>
2021-03-15 15:46:06 -0700

    removes SVProgressHUD, resolves assumed imports from Objc-bridge

--- data-collection/data-collection/Utilities/Progress View Controller/ProgressViewController.swift
+++ data-collection/data-collection/Utilities/Progress View Controller/ProgressViewController.swift
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import Foundation
+import UIKit
 
 extension UIViewController: GlobalProgressDisplayable {
     func showProgress(_ message: String, duration: TimeInterval? = nil) {
